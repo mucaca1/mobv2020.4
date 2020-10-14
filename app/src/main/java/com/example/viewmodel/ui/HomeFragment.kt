e37ecefe12833f2c5c7321ba3357afe980e8b90e
Maros <cavojsky.maros@gmail.com>
2020-10-14 09:50:25 +0200

    commit

--- app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
+++ app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
@@ -5,45 +5,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import com.example.viewmodel.R
+import com.example.viewmodel.databinding.FragmentHomeBinding
 import com.example.viewmodel.ui.viewModels.HomeViewModel
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+        binding.lifecycleOwner = this
+        binding.model = homeViewModel
 
-        //TODO: 1. prerobit na databinding
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        //TODO: 2. nahradit observer databindingom v xml
-        homeViewModel.word.observe(viewLifecycleOwner) { words_text.text = it }
-
-        //TODO: 5. nahradit listener databindingom v xml
-        word_btn.setOnClickListener { changeWord() }
-
-        //TODO: 6. umoznit navigaciu do DatabaseFragmentu po stlaceni tlacidla
-    }
-
-    //TODO: 5. odstanit funkciu pomocou databindingu v xml
-    private fun changeWord() {
-        val word = word_input.text.toString()
-        if (word.isNotEmpty()) {
-            homeViewModel.changeWord(word)
-            word_input.text.clear()
-        }
+        return binding.root
     }
 }
--- app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
@@ -5,15 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    //TODO: 3. urobit enkapsulaciu premennej word
     val word: MutableLiveData<String> = MutableLiveData()
-
-    //TODO: 4. urobit obojsmerny binding pre edittext
-
-    //TODO: 5. nahradit listener databindingom v xml
-    fun changeWord(word: String) {
-        this.word.postValue(word)
-    }
 
     //TODO: 6b.urobit transformaciu slova aby sa zobrazoval text "Slovo je: "
 }
