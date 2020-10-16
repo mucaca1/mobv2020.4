5c1bf7bff3af3393cac0f5d287fe845dddd08f2d
mucaca1 <matejkrc1@gmail.com>
2020-10-16 18:17:48 +0200

    Data binding
    - init

--- app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
+++ app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
@@ -8,11 +8,9 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import com.example.viewmodel.R
 import com.example.viewmodel.databinding.FragmentHomeBinding
 import com.example.viewmodel.ui.viewModels.HomeViewModel
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -35,21 +33,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO: 2. nahradit observer databindingom v xml
-        homeViewModel.word.observe(viewLifecycleOwner) { words_text.text = it }
-
-        //TODO: 5. nahradit listener databindingom v xml
-        word_btn.setOnClickListener { changeWord() }
-
         //TODO: 6. umoznit navigaciu do DatabaseFragmentu po stlaceni tlacidla
-    }
-
-    //TODO: 5. odstanit funkciu pomocou databindingu v xml
-    private fun changeWord() {
-        val word = word_input.text.toString()
-        if (word.isNotEmpty()) {
-            homeViewModel.changeWord(word)
-            word_input.text.clear()
-        }
     }
 }
--- app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
@@ -1,18 +1,25 @@
 package com.example.viewmodel.ui.viewModels
 
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    //TODO: 3. urobit enkapsulaciu premennej word
-    val word: MutableLiveData<String> = MutableLiveData()
+    val input: MutableLiveData<String> = MutableLiveData()
+    private val _word: MutableLiveData<String> = MutableLiveData()
 
-    //TODO: 4. urobit obojsmerny binding pre edittext
+    val word: LiveData<String>
+        get() {
+            return this._word
+        }
 
-    //TODO: 5. nahradit listener databindingom v xml
-    fun changeWord(word: String) {
-        this.word.postValue(word)
+    fun changeWord() {
+        input.value?.apply {
+            if (this.isNotEmpty()) {
+                _word.value = this
+            }
+        }
     }
 
     //TODO: 6b.urobit transformaciu slova aby sa zobrazoval text "Slovo je: "
