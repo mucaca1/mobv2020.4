f97ef2f8e1952490662763f92bf41ad4e078bf12
mucaca1 <matejkrc1@gmail.com>
2020-10-16 18:00:13 +0200

    Data binding
    - init

--- app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
+++ app/src/main/java/com/example/viewmodel/ui/HomeFragment.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import com.example.viewmodel.R
+import com.example.viewmodel.databinding.FragmentHomeBinding
 import com.example.viewmodel.ui.viewModels.HomeViewModel
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -16,14 +18,18 @@ import kotlinx.android.synthetic.main.fragment_home.*
 class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
 
+    lateinit var binging: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binging = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
-        //TODO: 1. prerobit na databinding
+        binging.lifecycleOwner = this
+        binging.homeModel = homeViewModel
+
+        return binging.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
